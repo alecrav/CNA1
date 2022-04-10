cac809709d3b9bb6e2586ff073a3c827a950ec59
Enrico-Benedettini <enrico.benedettini@usi.ch>
2022-04-10 13:00:35 +0200

    Accepting requests, not handling them yet

--- site/client.py
+++ site/client.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 #
-# This is a very simple CLIENT implementation for a very very simple
-# request/reply protocol.
-#
 import socket
 import sys
 
-if len(sys.argv) < 2:
-    # print('usage:',sys.argv[0],'<host> [<port> (default=1234)]')
-    exit(-1)
+print(len(sys.argv), sys.argv[0], sys.argv[0])
+# if len(sys.argv) < 2:
+#     # print('usage:',sys.argv[0],'<host> [<port> (default=1234)]')
+#     exit(-1)
 
-host_name = sys.argv[1]
+# host_name = sys.argv[1]
+host_name = 'localhost'
 
 if len(sys.argv) > 2:
     port_number = int(sys.argv[2])
 else:
-    port_number = 1234
+    port_number = 8083
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host_name, port_number))
-request = input('request> ')
+data = None
+request = ""
+while True:
+    data = sys.stdin.readline()
+    if (data == "\n"):
+        break
+    request = request + data
+# request = input('<request> ')
+
 s.send(request.encode('utf-8'))
 reply = s.recv(1024)
 # print('reply:', reply.decode('utf-8'))
 s.close()
-
--- site/server.py
+++ site/server.py
@@ -6,23 +6,30 @@
 
 import socket
 import sys
-import configparser
-
+from unittest import case
+from urllib import response
+from zoneinfo import ZoneInfo
+from datetime import datetime
+from pytz import reference
 
 if len(sys.argv) > 1:
     port_number = int(sys.argv[1])
 else:
-    port_number = 8080
+    port_number = 8083
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind(('', port_number))
 s.listen(1)
 
+file = "./site/vhosts.conf"
+test = open(file,"r")
+lines = test.read()
 
-configparser = configparser.ConfigParser()
-configFilePath = './vhosts.conf'
-configparser.read_file(open(configFilePath))
+server = 'Server: Group ComputerNotWorking Server'
+names = lines.split('\n')
+
+print(names)
 
 while True:
     print('Accepting connections')
@@ -31,41 +38,64 @@ while True:
     # parse and accept requests
     request = conn.recv(1024).decode()
     print('Request: ' + request)
-    request_list = request.split()
-    if request_list[0] == 'GET':
-        if request_list[1] == '/home.html':
-            request_list[1] = '/home.html'
-            continue
-        if request_list[1] == '/cgi-bin/hello.py':
-            conn.send(b'HTTP/1.1 200 OK\r\n\r\nHello World!')
+    # Split lines and organize matrix of lines
+    request_lines = request.split('\n')
+    request_entries = []
+    for i in range(len(request_lines)):
+        request_entries.append(request_lines[i].split())
+    
+    # Setting default values
+    resp = ""
+    version = "HTTP/1.0"
+    if (request_entries[0][2]):
+        version = request_entries[0][2]
+    
+    host = ""
+
+    if request_entries[1][1]:
+        host = request_entries[1][1]
+    
+    date = datetime.now().astimezone().strftime("%a, %d %b %Y %H:%M:%S %Z")
+
+    if request_entries[0][0] == "GET":
+        
+        if request_entries[0][1] ==  "/home.html":
+            # do something
+            conn.send("/" + host + "/home.html")
             conn.close()
             continue
-        else:
-            conn.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
-            conn.close()
-            continue
-
-    elif request_list[0] == 'PUT':
-        print('PUT request')
-
-    elif request_list[0] == 'DELETE':
+            # error handling
+        
+    elif request_entries[0][1] == "PUT":
+        print("PUT request")
+ 
+    elif request_entries[0][1] == "DELETE":
         # do something
         print("DELETE request")
 
-    elif request_list[0] == 'NTW22INFO':
+    elif request_entries[0][1] == "NTW22INFO":
+        admin = ""
+
+
+        content = "The administrator of " + host + " is " + admin +".\nYou can contact him at "
+        content_type = "text/plain"
+        length = 0 # TODO - retrieve length of the content
+
+        resp += version + '200 OK\n' + date + '\n' + server + length +  + content
         print("NTW22INFO request")
+        conn.send(resp)
 
     fields = resp.split("\r\n")
     fields = fields[1:] #ignore the GET / HTTP/1.1
     output = {}
     for field in fields:
-        key,value = field.split(':')#split each line by http field name and value
+        key,value = field.split(":")#split each line by http field name and value
         output[key] = value
-        
+        # 
     else:
-        print('Unknown request')
-        conn.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
-        
+        print("Unknown request")
+        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        # 
 
     
 # conn.close() HTTP/1.1 ??
